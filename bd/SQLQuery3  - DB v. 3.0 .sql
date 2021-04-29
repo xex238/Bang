1208982625c48f4fcfeb05fc62470623e7fa3eb9
Savalandr <savandr11111@mail.ru>
2021-04-29 21:23:02 +0300

    Update BD
    Полностью переработана структура базы данных. Полностью переработана ER модель. Переписан скрипт. Добавлена часть триггеров. (Текущая актуальная версия v. 3.0)

---
+++ bd/SQLQuery3  - DB v. 3.0 .sql
@@ -0,0 +1,481 @@
+﻿USE [Test_3]
+GO
+/*-----------------------------------------------------------------*/
+/*---------------------------TABLES-------------------------------*/
+/*-----------------------------------------------------------------*/
+CREATE TABLE [dbo].[Player](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[weapon_ID] [int] NOT NULL,
+	[player_move] [bit] NOT NULL,
+	[win] [bit] NOT NULL,
+	[character_ID] [int] NOT NULL,
+	[role_ID] [int] NOT NULL,
+	[user_ID] [int] NOT NULL,	
+	[room_ID][int] NULL
+)
+GO
+ALTER TABLE [dbo].[Player]
+ADD CONSTRAINT [PK_Player_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+CREATE TABLE [dbo].[Weapon](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](50) NOT NULL,
+	[base_weapon] [bit] NOT NULL,
+	[bang_player] [bit] NOT NULL,
+	[firing_range] [int] NOT NULL,
+	[endless_bang] [bit] NOT NULL
+)
+GO
+ALTER TABLE [dbo].[Weapon]
+ADD CONSTRAINT [PK_Weapon_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+CREATE TABLE [dbo].[Character](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[characters_ID] [int] NOT NULL,
+	[alive] [bit] NOT NULL,
+	[lives][int] NOT NULL
+
+)
+GO
+ALTER TABLE [dbo].[Character]
+ADD CONSTRAINT [PK_Character_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+CREATE TABLE [dbo].[Characters](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](50) NOT NULL,
+	[card_image] [bit] NOT NULL,
+	[card_back] [bit] NOT NULL,
+	[description][nvarchar](500) NOT NULL,
+	[max_lives] [int] NOT NULL
+)
+GO
+ALTER TABLE [dbo].[Characters]
+ADD CONSTRAINT [PK_Characters_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+CREATE TABLE [dbo].[Role](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[roles_ID] [int] NOT NULL,
+	[status] [int] NOT NULL
+)
+GO
+ALTER TABLE [dbo].[Role]
+ADD CONSTRAINT [PK_Role_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+CREATE TABLE [dbo].[Roles](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[name] [int] NOT NULL,
+	[card_image] [bit] NOT NULL,
+	[card_back] [bit] NOT NULL,
+	[description] [nvarchar](500) NOT NULL
+
+)
+GO
+ALTER TABLE [dbo].[Roles]
+ADD CONSTRAINT [PK_Roles_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+CREATE TABLE [dbo].[User](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[e-mail] [nvarchar](50) NOT NULL,
+	[password] [nvarchar](50) NOT NULL,
+	[nick] [nvarchar](50) NOT NULL,
+	[achievements_ID] [int] NULL
+)
+GO
+ALTER TABLE [dbo].[User]
+ADD CONSTRAINT [PK_User_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+CREATE TABLE [dbo].[Achievements](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[single_player_finished] [int] NOT NULL,
+	[single_player_unfinished] [int] NOT NULL,
+	[single_player_wins] [int] NOT NULL,
+	[single_player_defeats] [int] NOT NULL,
+	[single_player_wins_percentage] [real] NULL,	
+	[multi_player_finished] [int] NOT NULL,
+	[multi_player_wins] [int] NOT NULL,
+	[multi_player_defeats] [int] NOT NULL,
+	[multi_player_wins_percentage] [real] NULL
+)
+GO
+ALTER TABLE [dbo].[Achievements]
+ADD CONSTRAINT [PK_Achievements_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+CREATE TABLE [dbo].[Room](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[status] [int] NOT NULL,
+	[play_time] [time] NOT NULL,
+	[count_of_players] [int] NOT NULL,
+	[dropping_ID] [int] NULL,
+	[deck_ID] [int] NULL
+)
+GO
+ALTER TABLE [dbo].[Room]
+ADD CONSTRAINT [PK_Room_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+CREATE TABLE [dbo].[Dropping](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[count_of_cards] [int] NOT NULL
+)
+GO
+ALTER TABLE [dbo].[Dropping]
+ADD CONSTRAINT [PK_Dropping_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+CREATE TABLE [dbo].[Deck](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[count_of_cards] [int] NOT NULL
+)
+GO
+ALTER TABLE [dbo].[Deck]
+ADD CONSTRAINT [PK_Deck_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+CREATE TABLE [dbo].[Card](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[cards_ID] [int] NOT NULL,
+	[player_ID] [int] NULL,
+	[room_ID] [int] NOT NULL,
+	[card_location] [int] NOT NULL,
+	[index_number] [int] NOT NULL
+
+)
+GO
+ALTER TABLE [dbo].[Card]
+ADD CONSTRAINT [PK_Card_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+
+CREATE TABLE [dbo].[Cards](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](50) NOT NULL,
+	[card_image] [bit] NOT NULL,
+	[card_back] [bit] NOT NULL,
+	[description] [nvarchar](500) NOT NULL,
+	[color] [nvarchar](50) NOT NULL,
+	[suit] [nvarchar](50) NOT NULL,
+	[rating] [nvarchar](50) NOT NULL
+
+)
+GO
+ALTER TABLE [dbo].[Cards]
+ADD CONSTRAINT [PK_Cards_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+
+/*-----------------------------------------------------------------*/
+/*---------------------TABLES NOT REFLECTED -----------------------*/
+/*------------------------IN THE MODEL-----------------------------*/
+
+
+CREATE TABLE [dbo].[Friends_User-User](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[User1_ID] [int] NOT NULL,
+	[User2_ID] [int] NOT NULL
+)
+GO
+ALTER TABLE [dbo].[Friends_User-User]
+ADD CONSTRAINT [PK_Friends_User-User_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+
+CREATE TABLE [dbo].[Invitations_User-User](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[User1_ID] [int] NOT NULL,
+	[User2_ID] [int] NOT NULL,
+	[room_ID] [int] NOT NULL
+)
+GO
+ALTER TABLE [dbo].[Invitations_User-User]
+ADD CONSTRAINT [PK_Invitations_User-User_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+
+CREATE TABLE [dbo].[BlackList_User-User](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[User1_ID] [int] NOT NULL,
+	[User2_ID] [int] NOT NULL
+)
+GO
+ALTER TABLE [dbo].[BlackList_User-User]
+ADD CONSTRAINT [PK_BlackList_User-User_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+/*-----------------------------------------------------------------*/
+/*---------------------TABLES WITHOUT LINKS -----------------------*/
+/*-----------------------------------------------------------------*/
+
+
+CREATE TABLE [dbo].[Lives](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[image] [bit] NOT NULL
+)
+
+GO
+ALTER TABLE [dbo].[Lives]
+ADD CONSTRAINT [PK_Lives_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+CREATE TABLE [dbo].[Rules](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[image] [bit] NOT NULL
+)
+GO
+ALTER TABLE [dbo].[Rules]
+ADD CONSTRAINT [PK_Rules_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+CREATE TABLE [dbo].[Tablet](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[image] [bit] NOT NULL
+)
+GO
+ALTER TABLE [dbo].[Tablet]
+ADD CONSTRAINT [PK_Tablet_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+
+CREATE TABLE [dbo].[Reference](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[image] [bit] NOT NULL
+)
+GO
+ALTER TABLE [dbo].[Reference]
+ADD CONSTRAINT [PK_Reference_ID] PRIMARY KEY CLUSTERED ([ID])
+GO
+
+/*-----------------------------------------------------------------*/
+/*--------------------------TABLES END ----------------------------*/
+/*-----------------------------------------------------------------*/
+
+
+/*-----------------------------------------------------------------*/
+/*-----------------------FOREIGN KEYS------------------------------*/
+/*-----------------------------------------------------------------*/
+
+
+/*-----------------------------------------------------------------*/
+/*------------------FOR TABLES NOT REFLECTED ----------------------*/
+/*-----------------------IN THE MODEL------------------------------*/
+
+ALTER TABLE [dbo].[Friends_User-User]  
+WITH CHECK ADD  CONSTRAINT [FK_Friends_User-User_User1_ID] FOREIGN KEY([User1_ID])
+REFERENCES [dbo].[User] ([ID])
+GO
+
+ALTER TABLE [dbo].[Friends_User-User]  
+WITH CHECK ADD  CONSTRAINT [FK_Friends_User-User_User2_ID] FOREIGN KEY([User2_ID])
+REFERENCES [dbo].[User] ([ID])
+GO
+
+ALTER TABLE [dbo].[Invitations_User-User]  
+WITH CHECK ADD  CONSTRAINT [FK_Invitations_User-User_User1_ID] FOREIGN KEY([User1_ID])
+REFERENCES [dbo].[User] ([ID])
+GO
+
+ALTER TABLE [dbo].[Invitations_User-User]  
+WITH CHECK ADD  CONSTRAINT [FK_Invitations_User-User_User2_ID] FOREIGN KEY([User2_ID])
+REFERENCES [dbo].[User] ([ID])
+GO
+
+ALTER TABLE [dbo].[Invitations_User-User]  
+WITH CHECK ADD  CONSTRAINT [FK_Invitations_User-User_room_ID] FOREIGN KEY([room_ID])
+REFERENCES [dbo].[Room] ([ID])
+GO
+
+ALTER TABLE [dbo].[BlackList_User-User]  
+WITH CHECK ADD  CONSTRAINT [FK_BlackList_User-User_User1_ID] FOREIGN KEY([User1_ID])
+REFERENCES [dbo].[User] ([ID])
+GO
+
+ALTER TABLE [dbo].[BlackList_User-User]  
+WITH CHECK ADD  CONSTRAINT [FK_BlackList_User-User_User2_ID] FOREIGN KEY([User2_ID])
+REFERENCES [dbo].[User] ([ID])
+GO
+
+/*-----------------------------------------------------------------*/
+/*------------------------FOR ATHER TABLES-------------------------*/
+/*-----------------------------------------------------------------*/
+
+
+ALTER TABLE [dbo].[Player]  
+WITH CHECK ADD  CONSTRAINT [FK_Player_weapon_ID] FOREIGN KEY([weapon_ID])
+REFERENCES [dbo].[Weapon] ([ID])
+GO
+
+ALTER TABLE [dbo].[Player]  
+WITH CHECK ADD  CONSTRAINT [FK_Player_character_ID] FOREIGN KEY([character_ID])
+REFERENCES [dbo].[Character] ([ID])
+GO
+
+ALTER TABLE [dbo].[Character]  
+WITH CHECK ADD  CONSTRAINT [FK_Character_characters_ID] FOREIGN KEY([characters_ID])
+REFERENCES [dbo].[Characters] ([ID])
+GO
+
+ALTER TABLE [dbo].[Player]  
+WITH CHECK ADD  CONSTRAINT [FK_Player_role_ID] FOREIGN KEY([role_ID])
+REFERENCES [dbo].[Role] ([ID])
+GO
+
+ALTER TABLE [dbo].[Role]  
+WITH CHECK ADD  CONSTRAINT [FK_Role_roles_ID] FOREIGN KEY([roles_ID])
+REFERENCES [dbo].[Roles] ([ID])
+GO
+
+ALTER TABLE [dbo].[Player]  
+WITH CHECK ADD  CONSTRAINT [FK_Player_user_ID] FOREIGN KEY([user_ID])
+REFERENCES [dbo].[User] ([ID])
+GO
+
+ALTER TABLE [dbo].[User]  
+WITH CHECK ADD  CONSTRAINT [FK_User_achievements_ID] FOREIGN KEY([achievements_ID])
+REFERENCES [dbo].[Achievements] ([ID])
+GO
+
+ALTER TABLE [dbo].[Player] 
+WITH CHECK ADD  CONSTRAINT [FK_Player_room_ID] FOREIGN KEY([room_ID])
+REFERENCES [dbo].[Room] ([ID])
+GO
+
+ALTER TABLE [dbo].[Room]  
+WITH CHECK ADD  CONSTRAINT [FK_Room_dropping_ID] FOREIGN KEY([dropping_ID])
+REFERENCES [dbo].[Dropping] ([ID])
+GO
+
+ALTER TABLE [dbo].[Room]  
+WITH CHECK ADD  CONSTRAINT [FK_Room_deck_ID] FOREIGN KEY([deck_ID])
+REFERENCES [dbo].[Deck] ([ID])
+GO
+
+ALTER TABLE [dbo].[Card]  
+WITH CHECK ADD  CONSTRAINT [FK_Card_player_ID] FOREIGN KEY([player_ID])
+REFERENCES [dbo].[Player] ([ID])
+GO
+
+ALTER TABLE [dbo].[Card]  
+WITH CHECK ADD  CONSTRAINT [FK_Card_room_ID] FOREIGN KEY([room_ID])
+REFERENCES [dbo].[Room] ([ID])
+GO
+
+ALTER TABLE [dbo].[Card]  
+WITH CHECK ADD  CONSTRAINT [FK_Card_cards_ID] FOREIGN KEY([cards_ID])
+REFERENCES [dbo].[Cards] ([ID])
+GO
+
+/*-----------------------------------------------------------------*/
+/*-----------------------FOREIGN KEYS END--------------------------*/
+/*-----------------------------------------------------------------*/
+
+/*for removing foreign key*/
+/*
+ALTER TABLE [dbo].[TABLE_NAME]   
+DROP CONSTRAINT [FK_NAME];   
+GO  
+*/
+
+/*for changing table field*/
+/*
+ALTER TABLE
+[dbo].[TABLE_NAME]
+ALTER COLUMN
+[COLUMN_NAME] 
+[TYPE] NOT NULL
+GO
+*/
+
+/*-----------------------------------------------------------------*/
+/*---------------------------REQUESTS------------------------------*/
+/*-----------------------------------------------------------------*/
+
+/*---------------------------REQUEST_1------------------------------*/
+
+/*Make the [User] field in the [achievements_ID] table - NULL*/
+/*
+ALTER TABLE
+[dbo].[User]
+ALTER COLUMN
+[achievements_ID]
+[int] NULL;
+GO
+*/
+
+
+/*Make the [single_player_wins_percentage] and [multi_player_wins_percentage] field in the [Achievements] table - NULL*/
+/*
+ALTER TABLE
+[dbo].[Achievements]
+ALTER COLUMN
+[single_player_wins_percentage]
+[real] NULL;
+GO
+ALTER TABLE
+[dbo].[Achievements]
+ALTER COLUMN
+[multi_player_wins_percentage]
+[real] NULL;
+GO
+*/
+
+
+/* Write a trigger for creating achievements when creating a user and linking it to the User table */
+CREATE TRIGGER [dbo].[Create user achievements]
+ON [dbo].[User]
+AFTER INSERT
+AS
+BEGIN
+INSERT INTO [Achievements] ([single_player_finished], [single_player_unfinished], [single_player_wins], [single_player_defeats], [multi_player_finished], [multi_player_wins], [multi_player_defeats])
+VALUES (0,0,0,0,0,0,0);
+
+declare @achievements_ID [int] = IDENT_CURRENT('Achievements')  
+
+BEGIN
+UPDATE [User]
+SET [achievements_ID]  = @achievements_ID where ID = IDENT_CURRENT('User')  
+END
+
+END
+
+
+/*Request for creating User*/
+INSERT INTO [User] ([e-mail], [password], [nick])
+VALUES ('user_1@mail.ru','qwerty123','Bang_player_1');
+
+
+/*---------------------------REQUEST_2------------------------------*/
+
+/* Write a trigger to create records in the "Dropping" and "Deck" tables when creating a Room */
+CREATE TRIGGER [dbo].[Create Dropping and Deck default record]
+ON [dbo].[Room]
+AFTER INSERT
+AS
+BEGIN
+INSERT INTO [Dropping] ([count_of_cards])
+VALUES (0);
+INSERT INTO [Deck] ([count_of_cards])
+VALUES (0);
+
+declare @Dropping_ID [int] = IDENT_CURRENT('Dropping')
+declare @Deck_ID [int] = IDENT_CURRENT('Deck')  
+
+BEGIN
+UPDATE [Room]
+SET [dropping_ID]  = @Dropping_ID where ID = IDENT_CURRENT('Room')
+UPDATE [Room]
+SET [Deck_ID]  = @Deck_ID where ID = IDENT_CURRENT('Room')  
+END
+
+END
+
+
+/*Request for creating room*/
+INSERT INTO [Room] ([status], [play_time], [count_of_players])
+VALUES (0, '0:0:0', 0);
