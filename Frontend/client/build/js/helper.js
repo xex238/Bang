ea25d3c44f674714783cd107207f9d3825e52afa
ConstReznikov <73704986+ConstReznikov@users.noreply.github.com>
2021-05-28 13:14:12 +0300

    Режимы игры

---
+++ Frontend/client/build/js/helper.js
@@ -0,0 +1,18 @@
+const popup = document.querySelector(".popup");
+const chooseplayer = document.querySelector(".chooseplayers");
+const exit = document.querySelector(".option3");
+const shower = () => {
+    chooseplayer.style.display = "block";
+  
+  
+  
+};
+const hider = () => {
+  chooseplayer.style.display = "none";
+
+
+
+};
+popup.addEventListener("click", shower);
+exit.addEventListener("click", hider);
+
