5e43eb97d9fee392d26f94adcdba75e47353538e
Дмитрий <dima040998@yandex.ru>
2021-06-07 14:05:00 +0300

    Update insert data BD script

---
+++ bd/Данные для БД/SQLQuery - insert script (v.1.3).sql
@@ -0,0 +1,435 @@
+/*-----------------------------------------------------------------*/
+/*------------------------ Базовые данные ------------------------*/
+/*-----------------------------------------------------------------*/
+
+/*-----------------------------------------------------------------*/
+/*--------------- DATA FOR TABLE [Cards] ---------------*/
+/*-----------------------------------------------------------------*/
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (1,	'bang', 'Br', 'C', '5')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (2,	'bang',	'Br', 'C', '7')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (3, 'bang', 'Br', 'D', '7')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (4, 'scope', 'Bl', 'S', 'A')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (5, 'barrel', 'Bl', 'S', 'K')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (6, 'general_store', 'Br', 'C', '9')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (7, 'missed', 'Br', 'S', '6')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (8, 'bang', 'Br', 'D', 'K')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (9, 'missed', 'Br', 'S', '3')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (10, 'volcanic', 'Bl', 'S', '10')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (11, 'stagecoach', 'Br', 'S', '9')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (12, 'saloon', 'Br', 'H', '5')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (13, 'schofield', 'Bl', 'S', 'K')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (14, 'bang', 'Br', 'C', '2')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (15, 'missed', 'Br', 'S', '5')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (16, 'missed', 'Br', 'C', '10')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (17, 'remington', 'Bl', 'C', 'K')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (18, 'cat_balou', 'Br', 'D', '9')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (19, 'bang', 'Br', 'D', '8')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (20, 'bang', 'Br', 'D', '5')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (21, 'bang', 'Br', 'C', '6')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (22, 'cat_balou', 'Br', 'D', 'J')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (23, 'gatling', 'Br', 'H', '10')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (24, 'schofield', 'Bl', 'C', 'J')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (25, 'beer', 'Br', 'H', '7')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (26, 'dynamite', 'Bl', 'H', '2')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (27, 'missed', 'Br', 'S', '4')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (28, 'bang', 'Br', 'D', '2')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (29, 'schofield', 'Br', 'C', 'Q')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (30, 'cat_balou', 'Br', 'D', '10')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (31, 'volcanic', 'Bl', 'C', '10')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (32, 'bang', 'Br', 'C', '4')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (33, 'bang', 'Br', 'D', 'Q')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (34, 'panic', 'Br', 'H', 'A')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (35, 'bang', 'Br', 'D', '4')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (36, 'bang', 'Br', 'C', '3')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (37, 'bang', 'Br', 'H', 'K')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (38, 'mustang', 'Bl', 'H', '9')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (39, 'bang', 'Br', 'D', '9')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (40, 'missed', 'Br', 'C', 'J')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (41, 'bang', 'Br', 'D', '6')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (42, 'beer', 'Br', 'H', 'J')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (43, 'panic', 'Br', 'D', '8')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (44, 'bang', 'Br', 'C', '9')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (45, 'duel', 'Br', 'D', 'Q')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (46, 'bang', 'Br', 'S', 'A')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (47, 'barrel', 'Bl', 'S', 'Q')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (48, 'panic', 'Br', 'H', 'Q')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (49, 'bang', 'Br', 'D', 'A')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (50, 'wells_fargo', 'Br', 'H', '3')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (51, 'stagecoach', 'Br', 'S', '9')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (52, 'duel', 'Br', 'C', '8')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (53, 'beer', 'Br', 'h', '6')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (54, 'missed', 'Br', 'S', '8')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (55, 'indians', 'Br', 'D', 'A')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (56, 'panic', 'Br', 'H', 'J')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (57, 'missed', 'Br', 'C', 'A')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (58, 'bang', 'Br', 'D', 'J')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (59, 'indians', 'Br', 'D', 'K')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (60, 'bang', 'Br', 'H', 'Q')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (61, 'bang', 'Br', 'D', '3')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (62, 'winchester', 'Bl', 'S', '8')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (63, 'rev_carabine', 'Bl', 'C', 'A')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (64, 'beer', 'Br', 'H', '10')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (65, 'jail', 'Bl', 'H', '4')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (66, 'duel', 'Br', 'S', 'J')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (67, 'bang', 'Br', 'C', '8')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (68, 'general_store', 'Br', 'S', 'Q')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (69, 'missed', 'Br', 'C', 'Q')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (70, 'bang', 'Br', 'D', '10')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (71, 'missed', 'Br', 'S', '7')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (72, 'missed', 'Br', 'C', 'K')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (73, 'cat_balou', 'Br', 'H', 'K')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (74, 'bang', 'Br', 'H', 'A')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (75, 'mustang', 'Bl', 'H', '8')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (76, 'missed', 'Br', 'S', '2')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (77, 'jail', 'Bl', 'S', '10')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (78, 'jail', 'Bl', 'S', 'J')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (79, 'beer', 'Br', 'H', '9')
+
+INSERT INTO [Cards] ([ID], [name], [color], [suit], [rating])
+VALUES (80, 'beer', 'Br', 'H', '8')
+
+/*-----------------------------------------------------------------*/
+/*------------------ DATA FOR TABLE [Characters] ------------------*/
+/*-----------------------------------------------------------------*/
+
+INSERT INTO [Characters] ([ID], [name], [max_lives])
+VALUES (1, 'bart_cassidy', 4)
+
+INSERT INTO [Characters] ([ID], [name], [max_lives])
+VALUES (2, 'black_jack', 4)
+
+INSERT INTO [Characters] ([ID], [name], [max_lives])
+VALUES (3, 'calamity_janet', 4)
+
+INSERT INTO [Characters] ([ID], [name], [max_lives])
+VALUES (4, 'el_gringo', 3)
+
+INSERT INTO [Characters] ([ID], [name], [max_lives])
+VALUES (5, 'jesse_jones', 4)
+
+INSERT INTO [Characters] ([ID], [name], [max_lives])
+VALUES (6, 'jourdonnais', 4)
+
+INSERT INTO [Characters] ([ID], [name], [max_lives])
+VALUES (7, 'kit_carison', 4)
+
+INSERT INTO [Characters] ([ID], [name], [max_lives])
+VALUES (8, 'lucky_duke', 4)
+
+INSERT INTO [Characters] ([ID], [name], [max_lives])
+VALUES (9, 'paul_regret', 3)
+
+INSERT INTO [Characters] ([ID], [name], [max_lives])
+VALUES (10, 'pedro_ramires', 4)
+
+INSERT INTO [Characters] ([ID], [name], [max_lives])
+VALUES (11, 'rose_doolan', 4)
+
+INSERT INTO [Characters] ([ID], [name], [max_lives])
+VALUES (12, 'sid_ketchum', 4)
+
+INSERT INTO [Characters] ([ID], [name], [max_lives])
+VALUES (13, 'slab_the_killer', 4)
+
+INSERT INTO [Characters] ([ID], [name], [max_lives])
+VALUES (14, 'suzy_lafayette', 4)
+
+INSERT INTO [Characters] ([ID], [name], [max_lives])
+VALUES (15, 'vulture_sam', 4)
+
+INSERT INTO [Characters] ([ID], [name], [max_lives])
+VALUES (16, 'willy_the_kid', 4)
+
+/*-----------------------------------------------------------------*/
+/*-------------------- DATA FOR TABLE [Roles] --------------------*/
+/*-----------------------------------------------------------------*/
+
+INSERT INTO [Roles] ([ID], [name])
+VALUES (1, 'sheriff')
+
+INSERT INTO [Roles] ([ID], [name])
+VALUES (2, 'outlaw')
+
+INSERT INTO [Roles] ([ID], [name])
+VALUES (3, 'renegate')
+
+INSERT INTO [Roles] ([ID], [name])
+VALUES (4, 'deputy')
+
+/*-----------------------------------------------------------------*/
+/*------------------------ Тестовые данные ------------------------*/
+/*-----------------------------------------------------------------*/
+
+/*-----------------------------------------------------------------*/
+/*--------------------- DATA FOR TABLE [User] ---------------------*/
+/*-----------------------------------------------------------------*/
+
+INSERT INTO [User] ([e-mail], [password], [nick])
+VALUES ('dima040998@yandex.ru', '12345', 'xex238')
+
+INSERT INTO [User] ([e-mail], [password], [nick])
+VALUES ('dima_n@gmail.ru', 'null', 'dima')
+
+INSERT INTO [User] ([e-mail], [password], [nick])
+VALUES ('maryana_e@gmail.ru', 'qwerty', 'maryana')
+
+INSERT INTO [User] ([e-mail], [password], [nick])
+VALUES ('saveliy_a@gmail.ru', 'qwerty12345', 'saveliy')
+
+INSERT INTO [User] ([e-mail], [password], [nick])
+VALUES ('kostya_r@gmail.ru', 'qwerty', 'kostya')
+
+INSERT INTO [User] ([e-mail], [password], [nick])
+VALUES ('lida_m@gmail.ru', 'secret', 'lida')
+
+INSERT INTO [User] ([e-mail], [password], [nick])
+VALUES ('tanya_a@gmail.ru', 'password', 'tanya')
+
+/*-----------------------------------------------------------------*/
+/*--------------------- DATA FOR TABLE [Room] ---------------------*/
+/*-----------------------------------------------------------------*/
+
+INSERT INTO [Room] ([status], [count_of_players], [max_count_of_players], [owner_ID])
+VALUES (1, 1, 4, 1)
+
+INSERT INTO [Room] ([status], [count_of_players], [max_count_of_players], [owner_ID])
+VALUES (1, 2, 6, 5)
+
+INSERT INTO [Room] ([status], [count_of_players], [max_count_of_players], [owner_ID])
+VALUES (1, 3, 4, 7)
+
+INSERT INTO [Room] ([status], [count_of_players], [max_count_of_players], [owner_ID])
+VALUES(1, 1, 4, 2)
+
+/*-----------------------------------------------------------------*/
+/*--------------------- DATA FOR TABLE [Role] ---------------------*/
+/*-----------------------------------------------------------------*/
+
+INSERT INTO [Role] ([roles_ID], [status])
+VALUES (2, 'close')
+
+INSERT INTO [Role] ([roles_ID], [status])
+VALUES (1, 'sheriff')
+
+INSERT INTO [Role] ([roles_ID], [status])
+VALUES (3, 'close')
+
+INSERT INTO [Role] ([roles_ID], [status])
+VALUES (2, 'close')
+
+INSERT INTO [Role] ([roles_ID], [status])
+VALUES (2, 'close')
+
+INSERT INTO [Role] ([roles_ID], [status])
+VALUES (3, 'close')
+
+INSERT INTO [Role] ([roles_ID], [status])
+VALUES (4, 'close')
+
+/*-----------------------------------------------------------------*/
+/*------------------- DATA FOR TABLE [Character] ------------------*/
+/*-----------------------------------------------------------------*/
+
+INSERT INTO [Character] ([characters_ID], [alive], [lives])
+VALUES (5, 1, 3)
+
+INSERT INTO [Character] ([characters_ID], [alive], [lives])
+VALUES (14, 1, 2)
+
+INSERT INTO [Character] ([characters_ID], [alive], [lives])
+VALUES (2, 0, 0)
+
+INSERT INTO [Character] ([characters_ID], [alive], [lives])
+VALUES (8, 1, 1)
+
+INSERT INTO [Character] ([characters_ID], [alive], [lives])
+VALUES (6, 1, 4)
+
+INSERT INTO [Character] ([characters_ID], [alive], [lives])
+VALUES (7, 1, 4)
+
+INSERT INTO [Character] ([characters_ID], [alive], [lives])
+VALUES (10, 1, 1)
+
+/*-----------------------------------------------------------------*/
+/*-------------------- DATA FOR TABLE [Player] --------------------*/
+/*-----------------------------------------------------------------*/
+
+INSERT INTO [Player] ([is_ready], [additional_attack_range], [additional_defence_range], [user_ID], [role_ID], [character_ID], [room_ID])
+VALUES (0, 0, 0, 1, 1, 1, 3)
+
+INSERT INTO [Player] ([is_ready], [additional_attack_range], [additional_defence_range], [user_ID], [role_ID], [character_ID], [room_ID])
+VALUES (0, 0, 0, 2, 2, 2, 3)
+
+INSERT INTO [Player] ([is_ready], [additional_attack_range], [additional_defence_range], [user_ID], [role_ID], [character_ID], [room_ID])
+VALUES (0, 0, 0, 3, 4, 4, 3)
+
+INSERT INTO [Player] ([is_ready], [additional_attack_range], [additional_defence_range], [user_ID], [role_ID], [character_ID], [room_ID])
+VALUES (0, 0, 0, 4, 3, 3, 1)
+
+INSERT INTO [Player] ([is_ready], [additional_attack_range], [additional_defence_range], [user_ID], [role_ID], [character_ID], [room_ID])
+VALUES (0, 0, 0, 5, 5, 5, 4)
+
+INSERT INTO [Player] ([is_ready], [additional_attack_range], [additional_defence_range], [user_ID], [role_ID], [character_ID], [room_ID])
+VALUES (0, 0, 0, 6, 6, 6, 2)
+
+INSERT INTO [Player] ([is_ready], [additional_attack_range], [additional_defence_range], [user_ID], [role_ID], [character_ID], [room_ID])
+VALUES (0, 0, 0, 7, 7, 7, 2)
